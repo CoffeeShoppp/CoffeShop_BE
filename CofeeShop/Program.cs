bff66566263c86bbd257165a218d5eef6cf5d563
Tran Minh Duc <155726158+MinhhDucc9@users.noreply.github.com>
2026-03-03 18:57:58 +0700

    fix FE domain

--- CofeeShop/Program.cs
+++ CofeeShop/Program.cs
@@ -32,12 +32,30 @@ namespace CofeeShop
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("FrontendPolicy", policy =>
+                {
+                    policy
+                        .WithOrigins(
+                            "http://127.0.0.1:5500",
+                            "http://localhost:5500"
+                        // thêm domain FE deploy, ví d?:
+                        // "https://your-frontend.vercel.app"
+                        )
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
+
             builder.Services.AddOpenApi();
 
 
 
             var app = builder.Build();
+
+            app.UseCors("FrontendPolicy");
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
